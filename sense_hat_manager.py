01de8cde43540b6b6295c7a245cafffa82482698
Yadunand <yadunut@gmail.com>
2017-08-06 22:01:54 +0800

    Made changes to manager

--- sense_hat_manager.py
+++ sense_hat_manager.py
@@ -9,8 +9,8 @@ green = (0, 255, 0)
 blue = (0, 0, 255)
 yellow = (255, 255, 0)
 nothing = (0, 0, 0)
-# Shows battery level
 
+# Shows battery level on the display
 def show_battery_level(battery):
     battery_pixels = int(math.floor(battery / 2))
     pixels_list = [green for x in range(battery_pixels)]
@@ -22,10 +22,32 @@ def show_battery_level(battery):
 def get_current_temperature():
     return sense.get_temperature()
 
+# Where status is a list of the current pitch, roll, yaw
+
+def get_cumulative_movement(status):
+    curr_orientation = list(sense.get_orientation())
+    """
+    A very Interesting function where first, it will return a list of the differences between current orientation and status,
+    then add the elements in the list and will return True if its greater than 20 or it will return False
+    """
+    result = True if sum(list(map(lambda x, y: abs(x - y), curr_orientation, status))) > 20 else False
+
+    return result, curr_orientation
+
 def sense_hat_main(battery):
     counter = 0
+    status = [0, 0, 0]
+    initial_temperature = get_current_temperature()
+    distance_travelled = 0
     while counter < 5:
         time.sleep(3)
         counter += 1
         show_battery_level(battery)
-        temperature = get_current_temperature()
+        temperature_diff = temperature - initial_temperature
+        is_moving, status = get_cumulative_movement(status)
+        if(not is_moving):
+            battery -= 1
+        elif (is_moving and temperature_diff > 0.05):
+            battery += 1
+            distance_travelled += 0.01
+    return battery, distance_travelled
